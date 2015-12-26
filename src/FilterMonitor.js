bcf2dbf6662d2632d7edc87bc8e190df8fbd4efb
Zalmoxisus <zalmoxisus@gmail.com>
2015-12-26 14:38:26 +0200

    Filter actions

--- src/FilterMonitor.js
+++ src/FilterMonitor.js
@@ -11,6 +11,22 @@ export default class FilterMonitor extends Component {
 
   render() {
     const { whitelist, blacklist, children, ...childProps } = this.props;
+
+    if (whitelist || blacklist) {
+      let { stagedActionIds, actionsById } = childProps;
+      stagedActionIds = stagedActionIds.filter(id => {
+        const action = actionsById[id].action;
+        return (
+          whitelist && whitelist.indexOf(action.type) !== -1 ||
+          blacklist && blacklist.indexOf(action.type) === -1
+        );
+      });
+      childProps = {
+        ...childProps,
+        stagedActionIds: stagedActionIds
+      };
+    }
+
     return cloneElement(children, childProps);
   }
 }
